5835bfaf4caaa30fcb15ad2a0cd67e54555abdef
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-11-23 20:47:57 +0800

    Initialize dashboard layout and page: display user on authenticated

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,9 @@
+import DashboardLayout from "~/layouts/Dashbord";
+
+export default function DashboardPage() {
+  return (
+    <DashboardLayout>
+      <h1>Hello you're login on our platform</h1>
+    </DashboardLayout>
+  );
+}
