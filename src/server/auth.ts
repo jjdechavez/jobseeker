e42611127a6d4e5525a9c4dc66fc910c84792867
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-11-23 20:22:22 +0800

    Setup github oauth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,7 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GitHubProvider from "next-auth/providers/github";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
@@ -50,9 +50,9 @@ export const authOptions: NextAuthOptions = {
   // @ts-expect-error
   adapter: DrizzleAdapter(db, sqliteTable),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GitHubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.
