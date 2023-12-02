f6ae27a30ed8151cd1c61f28a1dd3ccc53ca1266
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-12-02 19:36:46 +0800

    Updated jobseeker stack: cleanup

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,6 @@
 import { SSTConfig } from "sst";
-import { API } from "./stacks/JobseekerStack";
+import { ApiStack } from "./stacks/ApiStack";
+import { WebStack } from "./stacks/WebStack";
 
 export default {
   config(_input) {
@@ -9,6 +10,8 @@ export default {
     };
   },
   stacks(app) {
-    app.stack(API);
-  }
+    app.setDefaultRemovalPolicy(app.mode === "dev" ? "destroy" : "retain");
+
+    app.stack(ApiStack).stack(WebStack);
+  },
 } satisfies SSTConfig;
---
+++ stacks/ApiStack.ts
@@ -0,0 +1,49 @@
+import { StackContext, Api, Auth, Config } from "sst/constructs";
+
+export function ApiStack({ stack }: StackContext) {
+  const secrets = Config.Secret.create(
+    stack,
+    "DATABASE_URL",
+    "DATABASE_AUTH_TOKEN"
+  );
+
+  const api = new Api(stack, "api", {
+    defaults: {
+      function: {
+        bind: [secrets.DATABASE_URL, secrets.DATABASE_AUTH_TOKEN],
+      },
+    },
+    routes: {
+      "GET /": "packages/functions/src/lambda.handler",
+      "POST /migration": {
+        function: {
+          handler: "packages/functions/src/migrator.handler",
+          description: "This is the migrator function",
+        },
+      },
+      // "GET /todo": "packages/functions/src/todo.list",
+      // "POST /todo": "packages/functions/src/todo.create",
+    },
+  });
+
+  const auth = new Auth(stack, "auth", {
+    authenticator: {
+      handler: "packages/functions/src/auth.handler",
+    },
+  });
+
+  auth.attach(stack, {
+    api,
+    prefix: "/auth",
+  });
+
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+
+  return {
+    api,
+    DATABASE_URL: secrets.DATABASE_URL,
+    DATABASE_AUTH_TOKEN: secrets.DATABASE_AUTH_TOKEN,
+  };
+}
