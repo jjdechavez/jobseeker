81690f66cfdddc66eaccf499241876c6ab1126b6
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-12-05 21:10:27 +0800

    Added dark and light theme toggle

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button"
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   const [session, setSession] = useState<{
@@ -63,42 +69,46 @@ function App() {
   if (loading) return <div className="container">Loading...</div>;
 
   return (
-    <>
-      <Button>Click me</Button>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="card">
-        {session ? (
-          <div className="profile">
-            <p>Welcome {session.name}</p>
-            <img
-              src={session.avatarUrl}
-              style={{ borderRadius: "50%" }}
-              width={100}
-              height={100}
-              alt=""
-            />
-            <p>{session.email}</p>
-            <button type="button" onClick={signOut}>
-              Sign out
-            </button>
-          </div>
-        ) : (
-          <a
-            href={`${import.meta.env.VITE_APP_API_URL}/auth/github/authorize`}
-            rel="noreferrer"
-          >
-            <button>Sign in with Github</button>
-          </a>
-        )}
-      </div>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Card>
+        <CardHeader>
+          <CardTitle>Create Project</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+          <ModeToggle />
+        </CardHeader>
+        <CardContent>
+          {session ? (
+            <div className="profile">
+              <p>Welcome {session.name}</p>
+              <img
+                src={session.avatarUrl}
+                style={{ borderRadius: "50%" }}
+                width={100}
+                height={100}
+                alt=""
+              />
+              <p>{session.email}</p>
+              <button type="button" onClick={signOut}>
+                Sign out
+              </button>
+            </div>
+          ) : (
+            <Button asChild>
+              <a
+                href={`${
+                  import.meta.env.VITE_APP_API_URL
+                }/auth/github/authorize`}
+                rel="noreferrer"
+              >
+                <button>Sign in with Github</button>
+              </a>
+            </Button>
+          )}
+        </CardContent>
+      </Card>
+    </ThemeProvider>
   );
 }
 
