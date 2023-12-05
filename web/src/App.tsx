8e3f7ee18ba88cc85f49fd26e8180aa6c18cdcd5
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-12-05 20:44:19 +0800

    Initialize shadcn as UI

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button"
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -63,6 +64,7 @@ function App() {
 
   return (
     <>
+      <Button>Click me</Button>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
