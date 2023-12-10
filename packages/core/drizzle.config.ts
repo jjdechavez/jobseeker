dda3467c9697f00804f003a2469c67de70700f68
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-12-10 18:08:10 +0800

    Fixed Top-level await... on drizzle-kit generate

--- packages/core/drizzle.config.ts
+++ packages/core/drizzle.config.ts
@@ -1,12 +1,12 @@
-import { Config as SSTConfig } from "sst/node/config";
 import type { Config } from "drizzle-kit";
 
-const isProduction = SSTConfig.STAGE === "production";
+// Issue 'Top-level await is currently not supported with the "cjs" output format
+// Solution: Remove SST Config on drizzle.config.ts; due to Config is Async
 
 export default {
   out: "./src/drizzle/migrations",
   schema: "./src/drizzle/sql/schema.ts",
   verbose: true,
-  driver: isProduction ? "turso" : "libsql",
+  driver: process.env.IS_LOCAL ? "libsql" : "turso",
   strict: true,
 } satisfies Config;
